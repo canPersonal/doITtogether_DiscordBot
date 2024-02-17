1638f95af2900d7997d30baa4df0c53e83c93002
canPersonal <160254889+canPersonal@users.noreply.github.com>
2024-02-17 10:34:39 +0100

    Update webPageFunctions.py

--- webPageFunctions.py
+++ webPageFunctions.py
@@ -10,9 +10,28 @@ import math
 
 def create_poll(event):
     try:
+        setx BROWSERSTACK_USERNAME "canoluk_698K32" 
+        setx BROWSERSTACK_ACCESS_KEY "GzPZ7wLYhKdwdHybZ9PG" 
+        set BROWSERSTACK_USERNAME=canoluk_698K32
+        set BROWSERSTACK_ACCESS_KEY=GzPZ7wLYhKdwdHybZ9PG
 
         
-        browser = webdriver.HtmlUnitDriver()
+        # Define desired capabilities for BrowserStack
+        desired_capabilities = {
+            'browser': 'Chrome',
+            'browser_version': 'latest',
+            'os': 'Windows',
+            'os_version': '10',
+            'name': 'Your Test Name'  # Replace with a descriptive name for your test
+        }
+
+        # Set up WebDriver with BrowserStack
+        browser = webdriver.Remote(
+            command_executor=BROWSERSTACK_URL,
+            desired_capabilities=desired_capabilities,
+            username=BROWSERSTACK_USERNAME,
+            access_key=BROWSERSTACK_ACCESS_KEY
+        )
 
         # Create a new instance of the Firefox driver
         # browser = webdriver.Firefox()
