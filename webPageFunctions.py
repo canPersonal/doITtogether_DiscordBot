cb350b6ecba7239058a5f487e730e51938107d5b
canPersonal <160254889+canPersonal@users.noreply.github.com>
2024-02-17 10:44:46 +0100

    Update webPageFunctions.py

--- webPageFunctions.py
+++ webPageFunctions.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from datetime import datetime, timedelta
 import math
