43a6ba11f91a8239cff009127d77d617d79e42e4
canPersonal <160254889+canPersonal@users.noreply.github.com>
2024-02-17 10:13:22 +0100

    Update webPageFunctions.py

--- webPageFunctions.py
+++ webPageFunctions.py
@@ -8,18 +8,19 @@ import math
 
 def create_poll(event):
     try:
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("start-maximized")
-        chrome_options.add_argument("disable-infobars")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-application-cache')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument("--disable-dev-shm-usage")
+    # Create Firefox options
+    firefox_options = Options()
+    firefox_options.headless = True
+    firefox_options.add_argument("--start-maximized")
+    firefox_options.add_argument("--disable-infobars")
+    firefox_options.add_argument("--disable-extensions")
+    firefox_options.add_argument("--no-sandbox")
+    firefox_options.add_argument("--disable-application-cache")
+    firefox_options.add_argument("--disable-gpu")
+    firefox_options.add_argument("--disable-dev-shm-usage")
 
-        # Create a new instance of the Chrome driver
-        browser = webdriver.Chrome(options=chrome_options)
+    # Create a new instance of the Firefox driver
+    browser = webdriver.Firefox(options=firefox_options)
 
         # Load the webpage
         browser.get("https://framadate.org/create_poll.php?type=date&lang=en")
