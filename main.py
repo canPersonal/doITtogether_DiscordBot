74acc235940ca984f77f8900574e058157269b4f
canPersonal <160254889+canPersonal@users.noreply.github.com>
2024-02-17 11:55:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import bot
-import os
 
-os.system('cmd /k "start chromedriver.exe"')
+import subprocess
+
+cmd = 'start chromedriver.exe'
+time = subprocess.Popen (cmd, shell=True)
+
 
 if __name__=='__main__':
     # run the bot
