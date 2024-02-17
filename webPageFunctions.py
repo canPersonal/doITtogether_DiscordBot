e2a84a3d82000c18b091864920321ed6ea991334
canPersonal <160254889+canPersonal@users.noreply.github.com>
2024-02-17 10:20:18 +0100

    Update webPageFunctions.py

--- webPageFunctions.py
+++ webPageFunctions.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
